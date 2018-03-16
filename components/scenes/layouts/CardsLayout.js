e12d50be334183db774eedcefe1f7c15333c9ba8
Lawrence Ackner <lmackner@gmail.com>
2018-03-15 21:46:11 -0400

    fix cardlayout

--- components/scenes/layouts/CardsLayout.js
+++ components/scenes/layouts/CardsLayout.js
@@ -5,7 +5,7 @@ import {
     Text
 } from 'react-vr';
 
-import { Easing } from 'react-native';
+import {Easing} from 'react-native';
 
 
 import CardButton from './elements/CardButton.js';
@@ -21,7 +21,7 @@ class CardsLayout extends React.Component {
         this.state = {
             slideLeft: new Animated.Value(-1),
             fadeIn: new Animated.Value(0),
-            name:"",
+            name: "",
             showButton: false,
             color1: "#A482DF",
             color2: "#DBDAF1",
@@ -54,61 +54,61 @@ class CardsLayout extends React.Component {
     }
 
 
-
     render() {
-        let  myCards = [];
-        let  row1 =[];
-        let  row2 =[];
-        let  row3 =[];
-        let  row4 =[];
+        let myCards = [];
+        let row1 = [];
+        let row2 = [];
+        let row3 = [];
+        let row4 = [];
         let c = new Constants();
         let cu = new Cards();
-        let cardDim = {height:.5,width:.4,valueFont:.1,rankFont:.2};
-        let cards =this.props.data.cards;
-        if(cards != null){
+        let cardDim = {height: .5, width: .4, valueFont: .1, rankFont: .2};
+        let cards = this.props.data.cards;
+        if (cards != null) {
             myCards = cards;
-            row1 =this.props.data.row1;
-            row2 =this.props.data.row2;
-            row3 =this.props.data.row3;
-            row4 =this.props.data.row4;
+            row1 = this.props.data.row1;
+            row2 = this.props.data.row2;
+            row3 = this.props.data.row3;
+            row4 = this.props.data.row4;
         }
 
-        let yourCards = myCards.map((item,index) => {
+        let yourCards = myCards.map((item, index) => {
 
-            return <CardButton dim = {cardDim} key={index}  color={cu.getColor(item,this.props.state===3)} row={0}
-                               background={cu.getBackground(item,this.props.state===3)} state={this.props.state}
-                               clickable={this.props.state===3}
+            return <CardButton dim={cardDim} key={index} color={cu.getColor(item, this.props.state === 3)} row={0}
+                               background={cu.getBackground(item, this.props.state === 3)} state={this.props.state}
+                               clickable={this.props.state === 3}
                                bulls={cu.getBulls(item)} card={item} pickCard={this.props.pickCard}/>
         });
         let row1Cards =
-            row1.map((item,index) => {
+            row1.map((item, index) => {
 
-            return <CardButton dim = {cardDim} key={index}  color={cu.getColor(item,item.state === 1)} row={1}
-                               background={cu.getBackground(item,item.state === 1)} bulls={cu.getBulls(item)}
+                return <CardButton dim={cardDim} key={index} color={cu.getColor(item, item.state === 1)} row={1}
+                                   background={cu.getBackground(item, item.state === 1)} bulls={cu.getBulls(item)}
+                                   card={item} pickCard={this.props.pickCard} clickable={item.state === 1}/>
+            });
+        let row2Cards = row2.map((item, index) => {
+
+            return <CardButton dim={cardDim} color={cu.getColor(item, item.state === 1)} key={index} row={2}
+                               background={cu.getBackground(item, item.state === 1)} bulls={cu.getBulls(item)}
                                card={item} pickCard={this.props.pickCard} clickable={item.state === 1}/>
         });
-        let row2Cards = row2.map((item,index) => {
+        let row3Cards = row3.map((item, index) => {
 
-            return <CardButton dim = {cardDim} color={cu.getColor(item,item.state === 1)} key={index} row={2}
-                               background={cu.getBackground(item,item.state === 1)} bulls={cu.getBulls(item)}
-                               card={item} pickCard={this.props.pickCard} clickable={item.state === 1} />
-        });
-        let row3Cards = row3.map((item,index) => {
-
-            return <CardButton dim = {cardDim}  key={index} color={cu.getColor(item,item.state === 1)} card={item}  row={3}
-                               background={cu.getBackground(item,item.state === 1)} bulls={cu.getBulls(item)}
+            return <CardButton dim={cardDim} key={index} color={cu.getColor(item, item.state === 1)} card={item} row={3}
+                               background={cu.getBackground(item, item.state === 1)} bulls={cu.getBulls(item)}
                                pickCard={this.props.pickCard} clickable={item.state === 1}/>
         });
-        let row4Cards = row4.map((item,index) => {
+        let row4Cards = row4.map((item, index) => {
 
-            return <CardButton dim = {cardDim} key={index}  color={cu.getColor(item,item.state === 1)} row={4}
-                               background={cu.getBackground(item,item.state === 1)}  bulls={cu.getBulls(item)}
-                               card={item} pickCard={this.props.pickCard} clickable={item.state === 1} />
+            return <CardButton dim={cardDim} key={index} color={cu.getColor(item, item.state === 1)} row={4}
+                               background={cu.getBackground(item, item.state === 1)} bulls={cu.getBulls(item)}
+                               card={item} pickCard={this.props.pickCard} clickable={item.state === 1}/>
         });
 
 
         return (
-            <View>
+
+            <View style={{width: 2.5, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
 
                 <Animated.View
                     style={{
@@ -122,15 +122,11 @@ class CardsLayout extends React.Component {
                             {translateX: this.state.slideLeft},
                             {translateZ: -3}
                         ],
-                        marginTop: -0.3
+                        marginTop: 1
                     }}
                 >
-                    <View style={{marginTop: -0.09, width: 2.5, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                        <View style={{ margin: 0.01,  flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                            {yourCards}
-                        </View>
+                    {yourCards}
 
-                    </View>
                 </Animated.View>
                 <Animated.View
                     style={{
@@ -147,12 +143,8 @@ class CardsLayout extends React.Component {
                         marginTop: -0.3
                     }}
                 >
-                    <View style={{marginTop: -0.09, width: 2.5, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                        <View style={{ margin: 0.01,  flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                            {row1Cards}
-                        </View>
+                    {row1Cards}
 
-                    </View>
                 </Animated.View>
 
                 <Animated.View
@@ -170,12 +162,8 @@ class CardsLayout extends React.Component {
                         marginTop: -0.3
                     }}
                 >
-                    <View style={{marginTop: -0.09, width: 2.5, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                        <View style={{ margin: 0.01,  flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                            {row2Cards}
-                        </View>
+                    {row2Cards}
 
-                    </View>
                 </Animated.View>
                 <Animated.View
                     style={{
@@ -192,12 +180,8 @@ class CardsLayout extends React.Component {
                         marginTop: -0.3
                     }}
                 >
-                    <View style={{marginTop: -0.09, width: 2.5, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                        <View style={{ margin: 0.01,  flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                            {row3Cards}
-                        </View>
+                    {row3Cards}
 
-                    </View>
                 </Animated.View>
                 <Animated.View
                     style={{
@@ -214,20 +198,10 @@ class CardsLayout extends React.Component {
                         marginTop: -0.3
                     }}
                 >
-                    <View style={{marginTop: -0.09, width: 2.5, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                        <View style={{ margin: 0.01,  flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
-                            {row4Cards}
-                        </View>
 
-                    </View>
+                    {row4Cards}
+
                 </Animated.View>
-
-
-
-
-
-
-
 
 
             </View>
--- utils/constants.js
+++ utils/constants.js
@@ -3,11 +3,11 @@
  */
 
 class Constants {
-    Y_YOUR_CARDS = 2.75;
+    Y_YOUR_CARDS = 5;
     CARD_HEIGHT = .5;
 
-    GAP_4ROWS = .6;
-    INNER_GAP_4ROWS = .3;
+    GAP_4ROWS = .3;
+    INNER_GAP_4ROWS = .1;
     FIRST_ROW = this.Y_YOUR_CARDS - this.CARD_HEIGHT - this.GAP_4ROWS;
     SECOND_ROW = this.FIRST_ROW - this.CARD_HEIGHT - this.INNER_GAP_4ROWS;
     THIRD_ROW = this.SECOND_ROW - this.CARD_HEIGHT - this.INNER_GAP_4ROWS;
