a9d54a7177984f8e379aa167e122487ddb9b8703
Lawrence Ackner <lmackner@gmail.com>
2020-04-02 18:05:09 -0400

    Acquire ui

--- components/scenes/layouts/PanMuck.js
+++ components/scenes/layouts/PanMuck.js
@@ -7,9 +7,9 @@ import {
     VrButton
 } from 'react-vr';
 
-import CardSuitRank from './elements/CardSuitRank.js';
+
 import PlayingCard from './elements/PlayingCard.js';
-import CardTableLayout from "./CardTableLayout";
+
 
 //Layout
 class PanMuck extends React.Component {
@@ -51,11 +51,98 @@ class PanMuck extends React.Component {
                     height: this.props.h,
                     flexDirection: 'row',
                     alignItems: 'center',
-                    justifyContent: 'center',
-                    backgroundColor: "blue",
+                    justifyContent: 'flex-start',
+                    backgroundColor: "green",
 
                 }}>
+                    <View style={{
+                        width: this.props.w/2,
+                        height: this.props.h,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start'
 
+                    }}>
+
+                        <View style={{
+                         marginLeft:0
+
+                        }}>
+                            <PlayingCard index={133} sz={.52} suit={'s'} rank={6}/>
+                        </View>
+                        <View style={{
+                            alignItems: 'flex-start',
+                            justifyContent: 'flex-start',
+                            flexDirection: 'column',
+                            width: .194 * .52 + .08,
+                            height: .264 * .52
+                        }}>
+                            <View
+                                style={{
+                                    opacity: 1,
+                                    marginLeft: 0,
+                                    marginRight:.02,
+                                    marginBottom:.005,
+                                    flexDirection: 'row',
+                                    backgroundColor: "brown",
+                                    width: .194 * .52,
+                                    height: .264 * .25
+                                }}>
+
+
+                                <VrButton onClick={this.pick(this)} key={0}>
+                                    <Text
+                                        style={{
+                                            fontSize: .03,
+                                            textAlign: 'left',
+                                            marginTop: .008,
+                                            marginLeft: .015,
+                                            color:"black"
+                                        }}>
+                                        {"Pass"}
+                                    </Text>
+                                </VrButton>
+                            </View>
+                            <View
+                                style={{
+                                    opacity: 1,
+                                    marginLeft: 0,
+                                    marginRight:.02,
+                                    flexDirection: 'row',
+                                    backgroundColor: "brown",
+                                    width: .194 * .52,
+                                    height: .264 * .25
+                                }}>
+
+
+                                <VrButton onClick={this.pick(this)} key={0}>
+                                    <Text
+                                        style={{
+                                            fontSize: .03,
+                                            textAlign: 'left',
+                                            marginTop: .008,
+                                            marginLeft: .015,
+                                            color:"black"
+                                        }}>
+                                        {"Pickup"}
+                                    </Text>
+                                </VrButton>
+                            </View>
+
+                        </View>
+                    </View>
+                    <View style={{
+                        width: this.props.w/2,
+                        height: this.props.h,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: "orange",
+
+                    }}>
+
+
+                    </View>
 
                 </View>
 
